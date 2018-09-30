cd8a606509d7fc04ec59439071dd7607e81b36d9
dylan-7 <wuxiewang@gmail.com>
2018-09-30 15:10:49 +0800

    Fixed: unixstamp

--- dist/formatUnixTime.js
+++ dist/formatUnixTime.js
@@ -1,30 +1,25 @@
 import moment from 'moment';
 var formatUnixTime = function (t) {
-    if (moment.unix(+t).isValid()) {
-        return moment.unix(+t).format('HH:mm:ss');
+    var time = +t;
+    var hour = 0;
+    var minutes = 0;
+    var seconds = 0;
+    if (+time > 60) {
+        minutes = parseInt("" + time / 60, 10);
+        seconds = parseInt("" + time % 60, 10);
+        if (minutes > 60) {
+            hour = parseInt("" + minutes / 60, 10);
+            minutes = parseInt("" + minutes % 60, 10);
+        }
+    }
+    else if (+time < 60) {
+        seconds = parseInt("" + time, 10);
+    }
+    if (hour >= 24) {
+        return hour + ":" + moment("1970-01-01 00:" + minutes + ":" + seconds).format('mm:ss');
     }
     else {
-        var time = +t;
-        var hour = 0;
-        var minutes = 0;
-        var seconds = 0;
-        if (+time > 60) {
-            minutes = parseInt("" + time / 60, 10);
-            seconds = parseInt("" + time % 60, 10);
-            if (minutes > 60) {
-                hour = parseInt("" + minutes / 60, 10);
-                minutes = parseInt("" + minutes % 60, 10);
-            }
-        }
-        else if (+time < 60) {
-            seconds = parseInt("" + time, 10);
-        }
-        if (hour >= 24) {
-            return hour + ":" + moment("1970-01-01 00:" + minutes + ":" + seconds).format('mm:ss');
-        }
-        else {
-            return moment("1970-01-01 " + hour + ":" + minutes + ":" + seconds).format('HH:mm:ss');
-        }
+        return moment("1970-01-01 " + hour + ":" + minutes + ":" + seconds).format('HH:mm:ss');
     }
 };
 /**
--- lib/formatUnixTime.ts
+++ lib/formatUnixTime.ts
@@ -1,28 +1,24 @@
 import moment from 'moment';
 
 const formatUnixTime = function(t: number|string) {
-  if (moment.unix(+t).isValid()) {
-    return moment.unix(+t).format('HH:mm:ss');
+  const time: number = +t;
+  let hour: number = 0o0;
+  let minutes: number = 0o0;
+  let seconds: number = 0o0;
+  if (+time > 60) {
+    minutes = parseInt(`${time / 60}`, 10);
+    seconds = parseInt(`${time % 60}`, 10);
+    if (minutes > 60) {
+      hour = parseInt(`${minutes / 60}`, 10);
+      minutes = parseInt(`${minutes % 60}`, 10);
+    }
+  } else if (+time < 60) {
+    seconds = parseInt(`${time}`, 10);
+  }
+  if (hour >= 24) {
+    return `${hour}:${moment(`1970-01-01 00:${minutes}:${seconds}`).format('mm:ss')}`;
   } else {
-    const time: number = +t;
-    let hour: number = 0o0;
-    let minutes: number = 0o0;
-    let seconds: number = 0o0;
-    if (+time > 60) {
-      minutes = parseInt(`${time / 60}`, 10);
-      seconds = parseInt(`${time % 60}`, 10);
-      if (minutes > 60) {
-        hour = parseInt(`${minutes / 60}`, 10);
-        minutes = parseInt(`${minutes % 60}`, 10);
-      }
-    } else if (+time < 60) {
-      seconds = parseInt(`${time}`, 10);
-    }
-    if (hour >= 24) {
-      return `${hour}:${moment(`1970-01-01 00:${minutes}:${seconds}`).format('mm:ss')}`;
-    } else {
-      return moment(`1970-01-01 ${hour}:${minutes}:${seconds}`).format('HH:mm:ss');
-    }
+    return moment(`1970-01-01 ${hour}:${minutes}:${seconds}`).format('HH:mm:ss');
   }
 }
 
