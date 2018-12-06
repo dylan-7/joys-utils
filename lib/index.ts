3ae6fd90db6a41b0f564b5a1c443db9b44efa46a
dylan-7 <wuxiewang@gmail.com>
2018-12-06 18:52:16 +0800

    [A] Added: add toFixeder

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export { default as periodTime } from './periodTime';
 export { default as convert, Multiple, Divide, Odds } from './convert';
 export { default as formatUnixTime } from './formatUnixTime';
 export { default as totalToData } from './totalToData';
+export { default as toFixeder } from './utils';
