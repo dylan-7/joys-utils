e9125dc73ad6a276b2c27d2eae17d45dcbfbcd94
dylan-7 <wuxiewang@gmail.com>
2018-12-06 18:52:29 +0800

    [A] Added: add toFixeder

---
+++ dist/utils.js
@@ -0,0 +1,23 @@
+'use strict';
+import { gte } from 'lodash/fp';
+/**
+ * 实用小方法
+ *
+ * @version
+ * v2.2.0
+
+ * @example
+ * toFixeder(0.235, 2) => 0.24
+ */
+var toFixeder = function (num, n) {
+    var symbol = 1;
+    if (gte(num, 0)) {
+        // 符号为负
+        symbol = -1;
+        num *= -1;
+    }
+    var num2 = (Math.round(num * Math.pow(10, n)) / Math.pow(10, n) + Math.pow(10, -(n + 1))).toString().slice(0, -1);
+    return parseFloat("" + +num2 * symbol).toFixed(n);
+};
+/** utils */
+export default toFixeder;
---
+++ types/utils.d.ts
@@ -0,0 +1,12 @@
+/**
+ * 实用小方法
+ *
+ * @version
+ * v2.2.0
+
+ * @example
+ * toFixeder(0.235, 2) => 0.24
+ */
+declare const toFixeder: (num: number, n: number) => string;
+/** utils */
+export default toFixeder;
