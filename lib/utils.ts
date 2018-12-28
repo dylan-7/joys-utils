10a60587750028e92fad171c3c4e16364327b45c
dylan-7 <wuxiewang@gmail.com>
2018-12-28 18:18:19 +0800

    Fixed float

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,26 +1,39 @@
 'use strict';
 
-import { gte } from 'lodash/fp';
-
 /**
  * 实用小方法
  * 
  * @version
  * v2.2.0
-
  * @example
  * toFixeder(0.235, 2) => 0.24
  */
-const toFixeder = function(num: number, n: number) {
-  let symbol: number = 1
-  if (gte(num, 0)) {
-    // 符号为负
-    symbol = -1
-    num *= -1
+const toFixeder = function(n: number, length: number) {
+  var carry = 0; // 存放进位标志
+  let num;
+  let multiple; // num为原浮点数放大multiple倍后的数，multiple为10的length次方
+  let str = n + ''; // 将调用该方法的数字转为字符串
+  let dot: number = str.indexOf('.'); // 找到小数点的位置
+  if (+str.substring(dot + length + 1, 1) >= 5) {
+    carry = 1; // 找到要进行舍入的数的位置，手动判断是否大于等于5，满足条件进位标志置为1
   }
-  const num2: string = (
-    Math.round(num * Math.pow(10, n)) / Math.pow(10, n) + Math.pow(10, -(n + 1))).toString().slice(0, -1)
-  return parseFloat(`${+num2 * symbol}`).toFixed(n)
+  multiple = Math.pow(10, length); // 设置浮点数要扩大的倍数
+  num = Math.floor(n * multiple) + carry; // 去掉舍入位后的所有数，然后加上我们的手动进位数
+  var result = num / multiple + ''; // 将进位后的整数再缩小为原浮点数
+  dot = result.indexOf('.');
+  if (dot < 0) {
+    result += '.';
+    dot = result.indexOf('.');
+  }
+  
+  // 处理多次进位
+  var len = result.length - (dot + 1);
+  if (len < length) {
+    for (var i = 0; i < length - len; i++) {
+      result += 0;
+    }
+  }
+  return result;
 };
 
 /** utils */
--- types/utils.d.ts
+++ types/utils.d.ts
@@ -3,10 +3,9 @@
  *
  * @version
  * v2.2.0
-
  * @example
  * toFixeder(0.235, 2) => 0.24
  */
-declare const toFixeder: (num: number, n: number) => string;
+declare const toFixeder: (n: number, length: number) => string;
 /** utils */
 export default toFixeder;
